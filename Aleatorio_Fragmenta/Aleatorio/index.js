bcb8b0b6b90852a720329c6d3f173c9b92088e39
Jose Luis <jcarrilloh1900@alumno.ipn.mx>
2026-02-17 09:40:18 -0600

    Se agregan trabajos

---
+++ Aleatorio_Fragmenta/Aleatorio/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express(); //Contenedor de Endpoints o WS Restful
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async function (request, response) {
+
+    r ={
+      'message':'Nothing to send'
+    };
+
+    response.json(r);
+});
+
+app.get("/aleatorio", async function (request, response) {
+
+    const numero = Math.floor(Math.random() * 100) + 1; 
+
+    const r = {
+        aleatorio: numero
+    };
+
+    response.json(r);
+});
+
+
+app.listen(3000, function() {
+    console.log('Aplicación ejemplo, escuchando el puerto 3000!');
+});
